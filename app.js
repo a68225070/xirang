41287fd5829ce6befcf0ada15fed0cb9caed0974
a68225070 <a68225070@gmail.com>
2017-03-29 09:48:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-//var xmlparser = require('express-xml-bodyparser');
 
 var bodyParser = require("body-parser");
 require('body-parser-xml')(bodyParser);
@@ -9,7 +8,6 @@ require('body-parser-xml')(bodyParser);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('.html',require('ejs').__express);
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.xml({
@@ -22,8 +20,7 @@ app.use(bodyParser.xml({
 }));
 
 require('./index')(app);
-//var parse =xmlparser({type:'text/xml'});
-//app.use(parse);
+
 
 
 module.exports=app;
