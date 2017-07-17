cfcb9909a9a4de558347bac138ed6a4b6a6463af
a68225070 <a68225070@gmail.com>
2017-07-17 20:58:33 +0800

    add b1

--- get.sh
+++ get.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-fn="myapp"
-if [ $# -eq 1 ]; then  
-        fn=$1
-fi 
-echo "get dir:$fn"
-echo "rsync -avz -e ssh ubuntu@www.naja.club:~/$fn ./"
-exec rsync -avz --exclude-from='./exclude.list' -e ssh ubuntu@115.159.161.35:~/$fn ./
+a
